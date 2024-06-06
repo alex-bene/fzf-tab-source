c27f451648bb9288094f305f649f37926ef2511f
alex-bene <alexandrosbene@gmail.com>
2024-06-07 02:18:32 +0300

    remote T option without type; wider preview

--- sources/df.zsh
+++ sources/df.zsh
@@ -1,2 +1,2 @@
-# :fzf-tab:complete:(\\|*/|)df:argument-rest
-[[ $group != 'device label' ]] && grc --colour=on df -Th $word
+# :fzf-tab:complete:(\\|*/|)df:argument-rest --preview-window=70%:wrap
+[[ $group != 'device label' ]] && grc --colour=on df -h $word
