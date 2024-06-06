cd46f46bafe6d51e6fa97b396903e07ab1e1e9cd
alex-bene <alexandrosbene@gmail.com>
2024-06-07 02:18:52 +0300

    fix pkill -> pgrep process name

--- sources/pkill.zsh
+++ sources/pkill.zsh
@@ -1,2 +1,2 @@
-# :fzf-tab:complete:(\\|*/|)(pkill:o-argument-rest|killall:argument-1)
-grc --colour=on ps -C$word
+# :fzf-tab:complete:(\\|*/|)(pkill:o-argument-rest|killall:argument-1) --preview-window=60%
+grc --colour=on ps -C $(pgrep $word)
